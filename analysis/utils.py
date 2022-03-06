d4adb14f053188075111f64d979ac1a1399bcba0
nicholas-dinicola <nicholasdinicola@outlook.com>
2022-03-06 11:41:13 +0100

    Analysis Done

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,3 +1,10 @@
+from math import isnan
+from sklearn.neighbors import NearestNeighbors
+from random import sample
+from numpy.random import uniform
+import numpy as np
+from math import isnan
+
 # Utility funcs to add colours to dataframe
 def color_df(val): 
     if val == 0 or val == 1: 
@@ -10,3 +17,27 @@ def color_df(val):
         color= "green"
 
     return 'color: %s' % color
+
+# Hopkins test for clustering
+def hopkins_test(X):
+    d = X.shape[1]
+    n = len(X) # rows
+    m = int(0.1 * n) 
+    nbrs = NearestNeighbors(n_neighbors=1).fit(X.values)
+
+    rand_X = sample(range(0, n, 1), m)
+
+    ujd = []
+    wjd = []
+    for j in range(0, m):
+        u_dist, _ = nbrs.kneighbors(uniform(np.amin(X,axis=0),np.amax(X,axis=0),d).reshape(1, -1), 2, return_distance=True)
+        ujd.append(u_dist[0][1])
+        w_dist, _ = nbrs.kneighbors(X.iloc[rand_X[j]].values.reshape(1, -1), 2, return_distance=True)
+        wjd.append(w_dist[0][1])
+ 
+    H = sum(ujd) / (sum(ujd) + sum(wjd))
+    if isnan(H):
+        print(ujd, wjd)
+        H = 0
+ 
+    return H
